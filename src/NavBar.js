89470089250145c95ca3850e1149d804045e2624
YanjieHe <heyanjie0@outlook.com>
2019-02-23 13:35:38 -0500

    home

--- src/NavBar.js
+++ src/NavBar.js
@@ -34,10 +34,6 @@ class NavBar extends React.Component {
                             <a className="nav-link" onClick={() => this.props.history.push("/blog/about/")}>About</a>
                         </li>
                     </ul>
-                    <form className="form-inline my-2 my-lg-0">
-                        <input className="form-control mr-sm-2" type="text" placeholder="Search"/>
-                        <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-                    </form>
                 </div>
             </nav>
         );
